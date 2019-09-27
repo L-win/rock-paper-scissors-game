9f50569a3128696826c356024d8ec8ddfda5cff2
L-win <l-win@inbox.ru>
2019-09-27 21:35:02 +0300

    add: base game architecture

--- Game.py
+++ Game.py
@@ -11,9 +11,9 @@ class Game:
   def __init__(self, playerPoints, computerPoints):
     self.playerPoints = playerPoints
     self.computerPoints = computerPoints
-    # self.getPlayerChoice()
-    # self.getComputerChoice()
-    # self.round()
+    self.getPlayerChoice()
+    self.getComputerChoice()
+    self.round()
   
   def getPlayerChoice(self):
     print('== Make your choice ==\n1 - rock\n2 - paper\n3 - scissors\n4 - exit game')
@@ -35,3 +35,29 @@ class Game:
   def getComputerChoice(self):
     choice = random.randint(0,2)
     self.computerChoice = self.choices[choice]
+
+  def round(self):
+    print ('\n== Result ==')
+    for choice, greater in self.rules.items():
+      if self.playerChoice == choice:
+        if self.playerChoice == self.computerChoice:
+          print('DRAW\n')
+          print('Player ', self.playerPoints,' - ', self.computerPoints , ' Computer\n')
+          game = Game(self.playerPoints, self.computerPoints)
+        elif self.computerChoice == greater:
+          print('PLAYER WINS\n')
+          print('Player ', self.playerPoints+1,' - ', self.computerPoints , ' Computer\n')
+          game = Game(self.playerPoints+1, self.computerPoints)
+        else:
+          print('COMPUTER WINS\n')
+          print('Player ', self.playerPoints,' - ', self.computerPoints+1, ' Computer\n')
+          game = Game(self.playerPoints, self.computerPoints+1)
+
+# Display game title
+print()
+print('='*10, 'START', '='*10)
+print('Rock Paper Scissors.')
+print('Player vs Computer\n')
+
+# Start game with base scores
+game = Game(0,0)
